84fc7cccce94e7640881de34781ae26ba539c209
DevenderDhami <devender.dhami.99@gmail.com>
2025-03-10 12:46:43 +0530

    build

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -79,6 +79,7 @@ const AboutUsPage = () => {
             </p>
           </div>
 
+
           <div className="flex justify-center items-center w-full order-4 md:order-2">
             {loading ? (
               <Skeleton
